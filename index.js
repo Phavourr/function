4ec1c40de779021ac3bff06c327de7aba3d0240d
egejuru favour <egejurufavour@gmail.com>
2021-04-07 10:35:55 +0100

    commit

---
+++ index.js
@@ -0,0 +1,21 @@
+//for loop
+// let start =1;
+// let end=100;
+// let add = 10;
+// for (start=1; start <= end; start += add){
+//     console.log(start);
+// } 
+function count (start, end , add){
+    for (start=1; start<=end ; start+= add)
+    console.log(start)
+}
+count(1,100,10);
+//while loop
+// let start =1
+// let end = 100
+// let add = 10
+//  while(start<=end){
+//      console.log(start);
+//      start+= add
+//  }
+ 
